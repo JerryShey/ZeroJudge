2e133645e628f52a37d762027490532e91040fd7
Jerryshey <mymyopa@gmail.com>
2015-03-28 13:31:03 +0800

    update a015.cpp

---
+++ ZeroJudse/a015.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a, b;
+	while(scanf("%d %d", &a, &b) != EOF){
+		int ary[b][a], i , j;
+		for(i = 0; i < a; i++){
+			for(j = 0; j < b; j++){
+				scanf("%d", &ary[j][i]);
+			}
+		}
+		for(i = 0; i < b; i++){
+			for(j = 0; j < a; j++){
+				printf("%d ", ary[i][j]);
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
+
